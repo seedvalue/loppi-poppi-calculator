49817e35b94e92ecd9eb69dabdbd1f5d1f363857
me <me@me.com>
2025-10-30 15:11:12 +0300

    -- bootstraper

---
+++ calc/Assets/_Main/GameBootstrapper.cs
@@ -0,0 +1,20 @@
+using _Core.Services;
+using _Main._Core.Presenter;
+using _Main._Core.Views;
+using UnityEngine;
+
+namespace _Main
+{
+    public class GameBootstrapper : MonoBehaviour
+    {
+        [SerializeField] private CalculatorView calculatorView;
+        
+        private CalculatorPresenter _presenter;
+
+        private void Start()
+        {
+            var repository = new PlayerPrefsCalculatorRepository();
+            _presenter = new CalculatorPresenter(calculatorView, repository);
+        }
+    }
+}
